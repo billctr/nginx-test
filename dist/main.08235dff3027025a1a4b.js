b64bb553da0436a584542e8e90c7fbca44e15179
Gabriel Castro <gabriel.castro@hqrevenue.com>
2022-06-30 11:44:06 +0200

    Publish new version

---
+++ dist/main.08235dff3027025a1a4b.js
@@ -0,0 +1,2 @@
+console.log("Hello other world");
+//# sourceMappingURL=main.08235dff3027025a1a4b.js.map
--- dist/main.fb1b3e1e5bd1b105d164.js
+++
@@ -1,2 +0,0 @@
-console.log("Hello weird world");
-//# sourceMappingURL=main.fb1b3e1e5bd1b105d164.js.map
